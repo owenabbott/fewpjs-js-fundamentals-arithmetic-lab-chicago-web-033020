c408c552f47c4cf43d550d61d046da5a32e68dd9
owenabbott <ocabbott@gmail.com>
2020-05-15 09:28:02 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+let num1 = 2
+let num2 = 31
+let multiply = num1 * num2
+
+let random = Math.floor(Math.random()*2+1)
+
+let mod = 16 % 6
+
+let max = Math.max(1, 2, 3, 4, 20, 5)
